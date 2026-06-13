97d62299f1ff46556c387c123b51ef1ccfeae1a8
SehanAF <sehanaf4@gmail.com>
2026-06-13 19:21:09 +0700

    feat: add PWA

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,6 +9,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#D97706" />
+        <meta name="application-name" content="INZTAMOTO" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-title" content="INZTAMOTO" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
